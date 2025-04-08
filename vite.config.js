a73c0dc75e75a4f4d002081c28d7bea2010b0846
streesobrs <101034124+streesobrs@users.noreply.github.com>
2025-04-08 17:33:35 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,8 @@ import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
 export default defineConfig({
-  plugins: [vue()],
-  base: process.env.NODE_ENV === 'production' ? '/StreePortal/' : '/',
-  define: {
-    'import.meta.env.VITE_APP_VERSION': JSON.stringify(process.env.VITE_APP_VERSION || 'dev')
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src')
-    }
+  base: '/StreePortal/',
+  build: {
+    outDir: 'dist'
   }
 })
